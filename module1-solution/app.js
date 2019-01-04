aefc4d1038a88c7920e37f6eeaf627b517119b99
gjnambiar <gautam.j.nambiar@gmail.com>
2019-01-04 18:24:39 +0530

    Module 1 Solution errors corrected

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,27 +1,29 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+angular.module("LunchCheck", [])
+.controller("LunchCheckController", LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.dishes = ''
   $scope.message = ''
 
-  $scope.LunchCheck = function () {
-    if($scope.menu) {
-      var items = $scope.dishes.split(',');
-      if(items.length == 0) {
+  $scope.CheckLunch = function () {
+    //if($scope.menu) {
+      var $items = $scope.dishes.split(',');
+      if($items[0] == "") {
         $scope.message = "Please enter data first";
       }
-      else if(items.length < 4) {
+      else if($items.length < 4) {
         $scope.message = "Enjoy!"
       }
       else {
         $scope.message = "Too much!"
       }
-    }
+    //}
+
+  }
 
 }
 
